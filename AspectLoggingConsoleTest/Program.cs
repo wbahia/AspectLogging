755ac6288fc5e82c77dcbdc5e5a4e6c47aeb57e9
wbahia <wbahia@gmail.com>
2019-07-09 09:57:41 -0300

    Add files via upload

---
+++ AspectLoggingConsoleTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using AspectLogging;
+
+
+namespace AspectLoggingConsoleTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateError("PARAMETRO_CHAMADA_METODO");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+            Console.ReadKey();
+
+        }
+
+
+        [ExceptionWrapper(typeof(ApplicationException))]
+        public static void CreateError(string s)
+        {
+            try
+            {
+                throw new ApplicationException("EXCEPTION");
+            }
+            catch (Exception ex)
+            {
+                    
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            
+        }
+    }
+}
